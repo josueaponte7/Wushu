6daf3452d87171100a489a88f15af3fe80c6dbb6
Proyecto Maternidad <maternidadproyecto@gmail.com>
2014-08-09 08:27:23 -0430

    Listo

--- modulos/camcla.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-include("bd.php");
-$NueCla = $_POST["NueCla"];
-$result = $link->query("UPDATE usuarios SET Clave = '".$NueCla."' WHERE NRegistro = ".$_SESSION["CodUsuAct"]);
--- modulos/ip.php
+++
@@ -1,9 +0,0 @@
-<?php
-if (!empty($_SERVER['HTTP_CLIENT_IP'])){
-	echo $_SERVER['HTTP_CLIENT_IP'];
-}elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
-	echo $_SERVER['HTTP_X_FORWARDED_FOR'];
-}else{   
-    echo $_SERVER['REMOTE_ADDR'];
-}
-?>
